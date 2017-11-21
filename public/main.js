ef4a8b4385866fe8bd8a9fd54bbf7d48f8a68e9a
Alumno21 j401 <a21j401@campustls.local>
2017-11-21 09:53:11 -0500

    agrego

---
+++ public/main.js
@@ -0,0 +1,42 @@
+  $(document).ready(function (){
+      $('input[type="submit"]').on("click", function(event){
+        event.preventDefault();
+
+        var correo = $('#correo').val();
+        var pass = $('#pass').val();
+        var gender = $('input[name="gender"]:checked').val();
+        var interest = $('input[name="interest"]:checked');
+        var interests = new Array();
+
+      for (var i = 0; i < interest.length; i++) {
+        var temp_interest = interest[i];
+        interests.push(temp_interest.value);
+      }
+
+      var payload = {
+        'correo':correo,
+        'pass' :pass,
+        'gender': gender,
+        'interests': interests
+    }
+    $.post("/find",payload);
+    $.get("/find",payload);
+
+    });
+});
+
+
+
+    /*var last_name = $('#last_name').val();
+    $.get('find.html',{'last_name':last_name});
+    $.post('find.html',{'last_name':last_name});
+
+  var gender = $('input[name="gender"]:checked').val();
+    $.post('find.html',{'gender':gender});
+
+
+  var interest= $('input[name="interest"]:checked').map(function(_,el){ return $(el).val();
+  }).get();
+
+    $.post('find.html',{'interest':interest});
+  /*String checkboxValues = request.getParameter("Intereses");*/
